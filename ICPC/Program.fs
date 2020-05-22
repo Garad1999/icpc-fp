563f91cbe1de700179aaf9eb3e085590249f23c1
Garad1999 <g18w8475@ru.ac.za>
2020-05-22 09:48:52 +0200

    % work for osme reason duye to accidental but still working on making the program

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -1,10 +1,19 @@
 ﻿module ICPC
 open System
 
-(*type ListStorage = {
-    list : [string] 
-    }*)
+type ListStorage = {
+    basis : List<String> //can be used to hold the basic list of the words
+    beginning : List<String> //contais the word and the space and comma
+    ending : List<String> //contains the comma
+    }
 
+
+let stringCon input =
+    let rec listToString input holder = 
+        match input with
+        |[] -> holder
+        |x::rest -> listToString rest (holder+x)
+    listToString input ""
 
 let printList mainlist (* : List<String> *)= //Check outputstatement
     
@@ -20,7 +29,25 @@ let printList mainlist (* : List<String> *)= //Check outputstatement
 
 let toList input= 
     let spaceSplit = (fun (x:string) -> Seq.toList (x.Split ' '))
-    spaceSplit input //COnverts To List
+    spaceSplit input //COnverts To List at " "
+
+let commaToList input= 
+    let newInput = stringCon input
+    printfn ("%s") newInput
+    let spaceSplit = (fun (x:string) -> Seq.toList (x.Split ',') )
+    spaceSplit newInput
+
+let periodToList input= 
+    let newInput = stringCon input
+    printfn ("%s") newInput
+    let spaceSplit = (fun (x:string) -> Seq.toList (x.Split '.') )
+    spaceSplit newInput
+
+let incorp input =
+    let v = toList input
+    let x = commaToList v
+    periodToList x
+    
 
 let toChar input = 
     let toChar = (fun (x:string) -> Seq.toList x)
@@ -39,10 +66,9 @@ let commaChecker input =
 
     toEnd (toChar input) //Can modify for spaces, returns a boolean //Checks the word if there is a comma if there isnt return false
     
-//let ListToString listToString=
-   // listToString.ToString //essentually toString method everywhere this is used .toString can work
 
-let commaCutter input= //returns List without comma
+
+(*let commaCutter input= //returns List without comma
     let x = toChar input
     let rec cutsCommaOut wordToCut=
         match wordToCut with
@@ -51,33 +77,35 @@ let commaCutter input= //returns List without comma
             match y with
             | ',' -> rest
             | _ -> y::(cutsCommaOut rest)
-    cutsCommaOut x
+    cutsCommaOut x*)
 
 
-let tester input=
+(*let tester input=
     let newlist = toList input
     let rec idividual input =
         match input with
         | [] -> []
         | x::input -> 
             commaCutter x 
-    idividual newlist
+    idividual newlist*)
+let construction input=
+    let checker :ListStorage = {basis = []; ending = []; beginning = []}
+    let update = {checker with ending = toList input; basis = incorp input}
+    update
 
-let commaSprinkler (input : string) =
-
-
-    let x = commaCutter input
-    let y = x.ToString()
-    let result = toList y
-    printList result
-
-
+let commaSprinkler input =
+    
+    let y = construction input
+    printfn ("%s") (y.ToString())
+    let x = stringCon (toList input)
+    printfn ("%s") (x)
 
 let rivers input =
     failwith "Not implemented"
 
 [<EntryPoint>]
 let main argv =
-    let x = commaSprinkler "HELLO, my, name, is, Garad"
+    let x = commaSprinkler "HELLO my, name, is, Garad"
+    
 
     0 // return an integer exit code
